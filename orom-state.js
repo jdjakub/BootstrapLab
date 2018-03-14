09e0750ccfcd1b0397c7b6bb67453065bd4b2546
jdjakub <16978710+jdjakub@users.noreply.github.com>
2018-03-14 22:31:31 +0000

    Orom-state is now an invisib--ahem, "unobtrusive", substrate.

--- orom-state.js
+++ orom-state.js
@@ -7,6 +7,23 @@
    What follows is NOT a substitute for building up the system step-by-step.
 */
 
+id_to_obj = [];
+
+function object(id) {
+  current = {};
+  id_to_obj[id] = current;
+}
+
+function state(k, v) {
+  current[k] = v;
+}
+
+function dom(tag, str) {
+  return str;
+}
+
+compile = src => new Function('return '+src)();
+
 // Generated using describeInJavascript() - see orom.js
 object(1);
 state('id', dom('INPUT', '1'));
@@ -17,6 +34,7 @@ state('-addMethod', dom('INPUT', '4'));
 state('-lookup', dom('INPUT', '5'));
 state('-allocate', dom('INPUT', '6'));
 state('-delegated', dom('INPUT', '8'));
+vtable_vt = current;
 object(2);
 state('id', dom('INPUT', '2'));
 state('vtable', dom('INPUT', '1'));
@@ -40,6 +58,7 @@ state('id', dom('INPUT', '5'));
 state('vtable', dom('INPUT', '3'));
 state('name', dom('INPUT', 'vtable.lookup'));
 state('code', dom('TEXTAREA', atob('ZnVuY3Rpb24ocmN2LCBuYW1lKSB7CiAgY29uc3Qgc3ltYm9sID0gJy0nK25hbWU7CiAgY29uc3QgaW1wbCA9IHN0YXRlKHJjdiwgc3ltYm9sKTsKICBjb25zdCBwYXJlbnQgPSBzdGF0ZShyY3YsICdwYXJlbnQnKTsKICBpZiAoaW1wbCA9PT0gJzAnICYmIHBhcmVudCAhPT0gJzAnKQogICAgcmV0dXJuIHNlbmQoZGVyZWYocGFyZW50KSwgJ2xvb2t1cCcsIG5hbWUpOwogIGVsc2UKICAgIHJldHVybiBpbXBsOwp9')));
+vtable_lookup = compile(current.code);
 object(6);
 state('id', dom('INPUT', '6'));
 state('vtable', dom('INPUT', '3'));
@@ -60,16 +79,18 @@ state('id', dom('INPUT', '9'));
 state('vtable', dom('INPUT', '3'));
 state('name', dom('INPUT', 'bind'));
 state('code', dom('TEXTAREA', atob('ZnVuY3Rpb24ocmN2LCBzZWxlY3RvcikgewogIGxldCBpbXBsOwogIGlmIChyY3YgPT09IHZ0YWJsZV92dCAmJiBzZWxlY3RvciA9PT0gJ2xvb2t1cCcpCiAgICBpbXBsID0gdnRhYmxlX2xvb2t1cChyY3YsIHNlbGVjdG9yKTsKICBlbHNlCiAgICBpbXBsID0gc2VuZChkZXJlZihzdGF0ZShyY3YsICd2dGFibGUnKSksICdsb29rdXAnLCBzZWxlY3Rvcik7CiAgaWYgKGltcGwgPT09ICcwJykKICAgIHRocm93IGBFbnRpdHkgJHtzdGF0ZShyY3YsICdpZCcpfSBkb2VzIG5vdCB1bmRlcnN0YW5kICR7c2VsZWN0b3J9YDsKICBlbHNlCiAgICByZXR1cm4gZGVyZWYoaW1wbCk7Cn0=')));
+bind = compile(current.code);
 object(10);
 state('id', dom('INPUT', '10'));
 state('vtable', dom('INPUT', '3'));
 state('name', dom('INPUT', 'send'));
 state('code', dom('TEXTAREA', atob('ZnVuY3Rpb24ocmN2LCBzZWxlY3RvciwgLi4uYXJncykgewogIGNvbnN0IGltcGwgPSBiaW5kKHJjdiwgc2VsZWN0b3IpOwogIHJldHVybiBjYWxsX2Z1bmMoaW1wbCwgcmN2LCAuLi5hcmdzKTsKfQ==')));
+send = compile(current.code);
 object(11);
 state('id', dom('INPUT', '11'));
 state('vtable', dom('INPUT', '3'));
 state('name', dom('INPUT', 'object.to-javascript'));
-state('code', dom('TEXTAREA', atob('ZnVuY3Rpb24ocmN2KSB7CiAgbGV0IHRycyA9IHJjdi5zdGF0ZVRhYi5xdWVyeVNlbGVjdG9yQWxsKCd0cicpOwogIGxldCBwYWlycyA9IEFycmF5LmZyb20odHJzKS5tYXAodHIgPT4gewogICAgbGV0IGtleSA9IHRyLmNsYXNzTmFtZTsKICAgIGxldCB2YWx1ZV9lbGVtID0gdHIucXVlcnlTZWxlY3RvcignLnZhbHVlJykuY2hpbGRyZW5bMF07CiAgICByZXR1cm4gW2tleSwgdmFsdWVfZWxlbV07CiAgfSk7CiAgbGV0IHN0YXRlX3NldHRlcnMgPSBwYWlycy5tYXAoKFtrLCB2ZWxlbV0pID0+IHsKICAgIGxldCB2YWx1ZV9qc19leHByID0gZG9tX3RvX2pzKHZlbGVtKTsKICAgIHJldHVybiBgc3RhdGUoJyR7a30nLCAke3ZhbHVlX2pzX2V4cHJ9KTtgOwogIH0pOwogIGxldCBwcmVhbWJsZSA9IGBvYmplY3QoJHtzdGF0ZShyY3YsICdpZCcpfSk7YDsKICBsZXQgY29kZSA9IHByZWFtYmxlICsgJ1xuJyArIHN0YXRlX3NldHRlcnMuam9pbignXG4nKTsKICByZXR1cm4gY29kZTsKfQ==')));
+state('code', dom('TEXTAREA', atob('ZnVuY3Rpb24ocmN2KSB7CiAgbGV0IHBhaXJzID0gT2JqZWN0LmVudHJpZXMocmN2KTsKICBsZXQgc3RhdGVfc2V0dGVycyA9IHBhaXJzLm1hcCgoW2ssIHZlbGVtXSkgPT4gewogICAgbGV0IHZhbHVlX2pzX2V4cHIgPSB2YWxfdG9fanModmVsZW0pOwogICAgcmV0dXJuIGBzdGF0ZSgnJHtrfScsICR7dmFsdWVfanNfZXhwcn0pO2A7CiAgfSk7CiAgbGV0IHByZWFtYmxlID0gYG9iamVjdCgke3N0YXRlKHJjdiwgJ2lkJyl9KTtgOwogIGxldCBjb2RlID0gcHJlYW1ibGUgKyAnXG4nICsgc3RhdGVfc2V0dGVycy5qb2luKCdcbicpOwogIHJldHVybiBjb2RlOwp9')));
 object(12);
 state('id', dom('INPUT', '12'));
 state('vtable', dom('INPUT', '1'));
@@ -92,3 +113,34 @@ state('id', dom('INPUT', '15'));
 state('vtable', dom('INPUT', '3'));
 state('name', dom('INPUT', 'mapping.dom-node'));
 state('code', dom('TEXTAREA', atob('ZnVuY3Rpb24obWFwcGluZykgewogIGxldCB0ciA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoJ3RyJyk7CiAgbGV0IHRkX2luID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgndGQnKTsKICBsZXQgdGRfb3V0ID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgndGQnKTsKICB0ci5hcHBlbmRDaGlsZCh0ZF9pbik7CiAgdHIuYXBwZW5kQ2hpbGQodGRfb3V0KTsKICAKICB0ZF9pbi5hcHBlbmRDaGlsZChzZW5kKGRlcmVmKHN0YXRlKG1hcHBpbmcsICdpbnB1dCcpKSwgJ2RvbS1ub2RlJywgbWFwcGluZykpOwogIHRkX291dC5hcHBlbmRDaGlsZChzZW5kKGRlcmVmKHN0YXRlKG1hcHBpbmcsICdvdXRwdXQnKSksICdkb20tbm9kZScsIG1hcHBpbmcpKTsKCiAgcmV0dXJuIHRyOwp9')));
+
+state = function(o, k, v) {
+  let old = o[k];
+  if (v !== undefined) {
+    o[k] = v;
+  }
+  if (old === undefined) old = '0';
+  return old;
+}
+
+call_func = function(f, ...args) {
+  f = compile(state(f, 'code'));
+  return f(...args);
+}
+
+deref = function(id) {
+  if (typeof(id) === 'object') return id;
+  const o = id_to_obj[id];
+  if (o === undefined) throw "There is no such object "+id;
+  else return o;
+}
+
+Entity = function() {
+  let o = {};
+  id_to_obj.push(o);
+  state(o, 'id', (id_to_obj.length-1).toString());
+  return o;
+}
+
+val_to_js = (str) => `atob('${btoa(str)}')`
+
