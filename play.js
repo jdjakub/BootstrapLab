dc05e4d7e980fe31215795e9969faf49a1a6c994
jdjakub <16978710+jdjakub@users.noreply.github.com>
2018-03-26 16:28:34 +0100

    True per-object messaging, aiming to be further than OROM

--- play.js
+++ play.js
@@ -1,10 +1,12 @@
+document.documentElement.style.height = '99%';
+
 body = document.body;
 body.style.border = '2px dashed red';
 body.style.margin = '0px';
+body.style.minHeight = '100%';
 
 body.addEventListener('mousedown', e => onMouseDown(e));
 onMouseDown = e => {
-  console.log(e);
   if (e.button === 0) {
     let lmb = nav('ui', 'mouse', 'left');
     send('pressed', lmb);
@@ -17,7 +19,6 @@ onMouseDown = e => {
 
 body.addEventListener('mouseup', e => onMouseUp(e));
 onMouseUp = e => {
-  console.log(e);
   if (e.button === 0) {
     let lmb = nav('ui', 'mouse', 'left');
     send('released', lmb);
@@ -30,9 +31,8 @@ onMouseUp = e => {
 
 body.addEventListener('mousemove', e => onMouseMove(e));
 onMouseMove = e => {
-  console.log(e);
   let cursor = nav('ui', 'mouse', 'cursor');
-  send('changed', cursor, {to: [e.offsetX, e.offsetY]});
+  send('changed', cursor, {absolute: [e.offsetX, e.offsetY]});
 };
 
 nav = function(...names) {
@@ -48,69 +48,102 @@ state = function(o, k, v) {
 }
 
 send = function(selector, receiver, context) {
-  // Decompose named-args form
-  if (receiver === undefined) {
-    let message = selector;
-    receiver = message.to;
-    selector = message.selector;
-    context = message.context;
+  // Short-form to long-form
+  let message;
+  if (receiver === undefined) message = selector;
+  else {
+    message = { from: null,
+                to: receiver,
+                selector: selector,
+                context: context };
   }
-  // Obtain method implementation.
-  let method_impl = null;
-  if (selector === 'method-impl'
-   && state(receiver, 'vtable') === receiver) {
-   // Special case -- cannot use message send.
-   // Use whatever the object decides
-   method_impl = state(receiver, 'method-impl');
-  } else {
-    // Obtain method impl from vtable
-    method_impl = send('method-impl',
-                       state(receiver, 'vtable'),
-                       { key: selector });
-  }
-  if (method_impl === undefined) throw "Does not understand: "+selector;
 
-  // Invoke method impl.
-  return method_impl(receiver, context);
+  // Let the receiver itself handle the message *however* it wants
+  let receive_message = state(message.to, 'receive-message');
+  return receive_message(message);
 };
+
+// Receive message using receiver's 'method-impl' function.
+// This function can return a method impl (function),
+// an object (which will receive a 'method-impl' message), or undefined
+function receive_via_obtain_impl_msg(msg) {
+  // Obtain method implementation
+  let method_impl;
+
+  // Expect receiver state to contain 'method-impl' function
+  let obtain_method_impl = state(msg.to, 'method-impl');
+  impl_or_delegate = obtain_method_impl(msg.to, msg.selector);
+  if (impl_or_delegate === undefined) throw ["Does not understand", msg];
+  else if (typeof(impl_or_delegate) === 'function') {
+    method_impl = impl_or_delegate;
+  } else { // Forward to delegate
+    method_impl = send('method-impl', impl_or_delegate, msg);
+    if (method_impl === undefined) throw ["Does not understand", msg];
+  }
+  
+  return method_impl(msg);
+}
+
+function method_impl_via_methods_dict_then_delegate(obj, selector) {
+  let method_dict = state(obj, 'methods');
+  if (method_dict === undefined) return state(obj, 'parent');
+  let impl = method_dict[selector];
+  if (impl === undefined) return state(obj, 'parent');
+  return impl;
+}
 
 universe = {};
-state(universe, 'vtable', universe);
+state(universe, 'receive-message', receive_via_obtain_impl_msg);
+state(universe, 'method-impl', method_impl_via_methods_dict_then_delegate);
 state(universe, 'methods', {});
-state(universe, 'directory', {});
-state(universe, 'method-impl', function(obj, ctx) {
-  return obj.methods[ctx.key];
-});
+state(universe, 'methods')['method-impl'] = function(msg) {
+  let obtain_method_impl = state(universe, 'method-impl');
+  return obtain_method_impl(msg.to, msg.context.selector);
+};
+state(universe, 'methods')['look-up'] = function(msg) {
+  if (msg.context.keys.length === 0) return msg.to;
 
-state(universe, 'methods')['look-up'] = function(obj, ctx) {
-  if (ctx.keys.length === 0) return obj;
-
-  let key = ctx.keys.shift();
-  let next = state(obj, 'directory')[key];
+  let key = msg.context.keys.shift();
+  let next = state(msg.to, 'directory')[key];
   if (next === undefined) throw "No such key "+key;
 
-  return send('look-up', next, ctx);
+  return send('look-up', next, msg.context);
 };
+state(universe, 'directory', {});
 
 universe.ui = {}
-state(universe.ui, 'vtable', universe);
+state(universe.ui, 'receive-message', receive_via_obtain_impl_msg);
+state(universe.ui, 'method-impl', method_impl_via_methods_dict_then_delegate);
+state(universe.ui, 'parent', universe);
 state(universe.ui, 'directory', {});
-
 state(universe, 'directory')['ui'] = universe.ui;
 
 universe.ui.mouse = {}
-state(universe.ui.mouse, 'vtable', universe);
+state(universe.ui.mouse, 'receive-message', receive_via_obtain_impl_msg);
+state(universe.ui.mouse, 'method-impl', method_impl_via_methods_dict_then_delegate);
+state(universe.ui.mouse, 'parent', universe);
 state(universe.ui.mouse, 'directory', {});
-
 state(universe.ui, 'directory')['mouse'] = universe.ui.mouse;
 
 universe.ui.mouse.left = {}
-state(universe.ui.mouse.left, 'vtable', universe);
+state(universe.ui.mouse.left, 'receive-message', receive_via_obtain_impl_msg);
+state(universe.ui.mouse.left, 'method-impl', method_impl_via_methods_dict_then_delegate);
+state(universe.ui.mouse.left, 'parent', universe);
+state(universe.ui.mouse.left, 'methods', {});
+state(universe.ui.mouse.left, 'methods')['pressed'] = function(msg) {};
+state(universe.ui.mouse.left, 'methods')['released'] = function(msg) {
+  console.log(msg);
+};
 state(universe.ui.mouse.left, 'directory', {});
-
 state(universe.ui.mouse, 'directory')['left'] = universe.ui.mouse.left;
 
 universe.ui.mouse.cursor = {};
-state(universe.ui.mouse.cursor, 'vtable', universe);
+state(universe.ui.mouse.cursor, 'receive-message', receive_via_obtain_impl_msg);
+state(universe.ui.mouse.cursor, 'method-impl', method_impl_via_methods_dict_then_delegate);
+state(universe.ui.mouse.cursor, 'parent', universe);
+state(universe.ui.mouse.cursor, 'methods', {});
+state(universe.ui.mouse.cursor, 'methods')['changed'] = function(msg) {
+  state(msg.to, 'value', msg.context.absolute);
+};
 state(universe.ui.mouse.cursor, 'directory', {});
 state(universe.ui.mouse, 'directory')['cursor'] = universe.ui.mouse.cursor;
