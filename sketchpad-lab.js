978d5199a8869679b8e9b0a7d5ece6f8e9ccfb76
jdjakub <16978710+jdjakub@users.noreply.github.com>
2018-05-20 19:42:16 +0100

    Refactored to give SVG shapes a basic level of autonomy

--- sketchpad-lab.js
+++ sketchpad-lab.js
@@ -42,27 +42,66 @@ sub = (as,bs) => {
 svg = svgel('svg', body, {width: body.offsetWidth*0.99, height: body.offsetHeight*0.99});
 svg.style.border = '2px dashed red';
 
+send = ({ to, selector }, context) => {
+  return to.receive({ recv: to, selector }, context);
+}
+
+svg_userData = (elem, obj) => state(elem, 'userData', obj);
+
+svg_userData(svg, {
+  receive: ({ recv, selector }, context) => {
+    if (selector === 'clicked') {
+      // Create SVG circle and route keyboard input "to it"
+      let e = context.dom_event;
+      let circ = svgel('circle', svg, {r: 15, fill: 'red'});
+      attribs(circ, {cx: e.offsetX, cy: e.offsetY});
+      let obj = create_circle(circ);
+      svg_userData(circ, obj);
+      // Route keyboard input "to" the circle
+      keyboard_focus = obj;
+    } else throw "No comprende "+selector;
+  }
+});
+
+create_circle = (c) => {
+  return {
+    svgel: c,
+    receive: ({ recv, selector }, context) => {
+      let e = context.dom_event;
+      if (selector === 'clicked') {
+        let circ = recv.svgel;
+        // Implement the initial conditions of the difference equation
+        // center @ t+1 - center @ t = pointer @ t+1 - pointer @ t
+        center_0 = [+circ.getAttribute('cx'), +circ.getAttribute('cy')];
+        pointer_0 = offset(e);
+        // Enable the maintenance of this equality
+        moving = circ;
+        keyboard_focus = recv;
+      } else if (selector === 'key-down') {
+        if (recv.str === undefined) { // Lazy initialise text line on key input
+          let [cx,cy] = [recv.svgel.getAttribute('cx'), recv.svgel.getAttribute('cy')];
+          // Place text baseline and start point at circle center
+          recv.str = svgel('text', svg, {x: cx, y: cy, font_size: 15, fill: 'black'});
+        }
+        if (e.key === 'Backspace') // Modify the SVG dumb-state
+          recv.str.textContent = recv.str.textContent.slice(0,-1);
+        else if (e.key === 'Enter') { // Use SVG dumb-state as JS source code
+          eval(recv.str.textContent);
+        } else if (e.key === 'v' && e.ctrlKey) { // Easy C+P, but no display newline
+          recv.str.textContent = typeof(dump) === 'string' ? dump : "";
+        } else if (e.key.length === 1) // Modify the SVG dumb-state
+          recv.str.textContent += e.key;
+      } else throw "No comprende "+selector;
+    }
+  };
+};
+
 svg.onmousedown = e => {
   // Two things have happened.
   // First, an external event has occurred.
   // Second, SVG has performed a spatial index and identified a shape at x,y.
-  if (e.target === svg) {
-    // Create SVG circle and route keyboard input "to it"
-    let circ = svgel('circle', svg, {r: 15, fill: 'red'});
-    attribs(circ, {cx: e.offsetX, cy: e.offsetY});
-    keyboard_focus = circ;
-  } else {
-    // Assume hit circle, previously created by above.
-    let circ = e.target;
-    // Implement the initial conditions of the difference equation
-    // center @ t+1 - center @ t = pointer @ t+1 - pointer @ t
-    center_0 = [+circ.getAttribute('cx'), +circ.getAttribute('cy')];
-    pointer_0 = offset(e);
-    // Enable the maintenance of this equality
-    moving = circ;
-    // Route keyboard input "to" the circle
-    keyboard_focus = circ;
-  }
+  let obj = svg_userData(e.target);
+  if (obj !== undefined) send({ to: obj, selector: 'clicked'}, {dom_event: e});
 };
 
 moving = undefined;
@@ -86,23 +125,7 @@ svg.onmouseup = e => {
 };
 
 dump = "";
-keyboard_focus = svg;
+keyboard_focus = svg_userData(svg);
 
-body.onkeydown = e => {
-  if (keyboard_focus.tagName !== 'svg') { // i.e. if circle
-    if (keyboard_focus.str === undefined) { // Lazy initialise text line on key input
-      let [cx,cy] = [keyboard_focus.getAttribute('cx'), keyboard_focus.getAttribute('cy')];
-      // Place text baseline and start point at circle center
-      keyboard_focus.str = svgel('text', svg, {x: cx, y: cy, font_size: 15, fill: 'black'});
-    }
-    if (e.key === 'Backspace') // Modify the SVG dumb-state
-      keyboard_focus.str.textContent = keyboard_focus.str.textContent.slice(0,-1);
-    else if (e.key === 'Enter') { // Use SVG dumb-state as JS source code
-      eval(keyboard_focus.str.textContent);
-    } else if (e.key === 'v' && e.ctrlKey) { // Easy C+P, but no display newline
-      keyboard_focus.str.textContent = typeof(dump) === 'string' ? dump : "";
-    } else if (e.key.length === 1) // Modify the SVG dumb-state
-      keyboard_focus.str.textContent += e.key;
-  }
-};
+body.onkeydown = e => send({ to: keyboard_focus, selector: 'key-down' }, {dom_event: e});
 
