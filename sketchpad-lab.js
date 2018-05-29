f7339db80f5eb9fb67b52133b14ba43f64ac3ea6
jdjakub <16978710+jdjakub@users.noreply.github.com>
2018-05-29 21:12:06 +0100

    Split into dispatch via dynamic lookup on message selector
    Now we can finally, if needed, modify response to messages at runtime.
    Even though it's a coarse level; further granularity can only come
    about by moving from JS source text to runtime JS object representation
    of state-changes, conditional branches and JS method calls.

--- sketchpad-lab.js
+++ sketchpad-lab.js
@@ -42,10 +42,17 @@ backg = svgel('rect', svg, {x: 0, y: 0,
                             fill: 'black'});
 
 send = ({ to, selector }, context) => {
-  if (typeof(to.receive) === 'function')
-    return to.receive({ recv: to, selector }, context || {});
-  else throw "No comprende "+selector;
-}
+  let next_code_path = to.receive || default_dyn_single_dispatch;
+  return next_code_path({ recv: to, selector }, context || {});
+};
+
+default_dyn_single_dispatch = ({ recv, selector }, context) => {
+  let selector_to_next_code_path = recv.vtable || default_vtable;
+  let next_code_path = selector_to_next_code_path[selector];
+  if (next_code_path === undefined) throw "No comprende "+selector;
+  
+  return next_code_path({ recv, selector }, context);
+};
 
 svg_userData = (elem, obj) => state(elem, 'userData', obj);
 
@@ -53,118 +60,58 @@ offset = e => [e.offsetX, e.offsetY]
 
 add = (as,bs) => {
   return as.map((a,k) => a + bs[k]);
-}
+};
 
 sub = (as,bs) => {
   return as.map((a,k) => a - bs[k]);
-}
-
-// extensional function
-efunc = (spec) => {
-  let f = { map: new Map(), inverse: new Map() };
-  for (let [input,output] of spec) {
-    efunc.set(f, input, output);
-  }
-  return f;
-}
-
-efunc.inverse = (f) => {
-  let a = Array.from; // ffs...
-  // JavaScript: where typing atob() and btoa() is acceptable
-  // but being able to call map() on a Map is one step too far...
-  let spec = a(f.inverse.entries()).map(([inp,outp]) => {
-    // OK. If we have a one-element set, unwrap it (**)
-    if (outp.size === 1) return [inp, a(outp.values())[0]]; // {i} --> i
-    // Otherwise, MUST return a (shallow) copy
-    else return [inp, new Set(outp)]; // {i1, i2, i3} --> {i1, i2, i3}
-  });
-  return efunc(spec);
 };
-
-efunc.get = (f, input) => {
-  return f.map.get(input);
-};
-
-efunc.set = (f, input, output) => {
-    // Currently, but not for long, f(input) = "old" output.
-    // When we make this no longer the case, input will no longer be in
-    // the set of things that map to old-output, i.e. its preimage.
-    let old_output = f.map.get(input);
-    if (old_output !== undefined) {
-      let preimage = f.inverse.get(old_output);
-      preimage.delete(input);
-    }
-    if (output === undefined)
-      f.map.delete(input);
-    else {
-      f.map.set(input, output);
-      // Now input is in the preimage of output.
-      let preimage = f.inverse.get(output);
-      if (preimage === undefined) {
-        preimage = new Set(); // Lazy initialise
-        f.inverse.set(output, preimage);
-      }
-      preimage.add(input);
-    }
-};
-
-{
-  some_pi_digits = efunc(Object.entries({one: 3, two: 1, three: 4, four: 1}));
-  inv = efunc.inverse(some_pi_digits);
-  efunc.get(inv, 1); // --> { "two", "four" }
-  efunc.get(inv, 4); // --> "three"
-  // Not recommended to have Sets as genuine outputs ... erk!
-  // (**) Might be better to not have un-wrapping functionality.
-}
-
 
 svg_userData(backg, {
-  receive: ({ recv, selector }, context) => {
-    // Q: can any of this behaviour be changed in-system?
-    // A: no, except by completely re-writing receive() in a single line
-    // This is because, in JS, one cannot go INSIDE functions and make
-    // piece-meal changes to their code.
-    // The function is an atomic black-box.
-    // Solution: at least break it up into separate functions.
-    // Eventually, break up into atomic JS state operations:
-    // STATE-COPY o1 [ k1 ] <-- o2 [ k2 ] where o1,k1,o2,k2 are names
-    // METHOD INVOCATION: o.f(a1 ... aN)
-    // (CONDITIONAL) SEQUENCE: to be decided, but seems to consist
-    // of an extensional function to new instruction
-    // e.g.      if (a < 0) goto L
-    //      else if (a = 0) goto E
-    //      else if (a > 0) goto G
-    // is just an extensional function from the 3 possible values of sign(a):
-    // let s = sign(a)
-    // goto ( put s through the function:
-    //       -1 |--> L
-    //        0 |--> E
-    //        1 |--> G )
-    // We have gone from INFINITE SET (values of a)
-    // to FINITE SET (values of sign(a))
-    // to NEXT STATE-CHANGE (infinite set?)
-    // In summary: EXPOSE THE SUBSTRATE, part of which is JS itself.
-    if (selector === 'clicked') {
+  // In JS, one cannot go INSIDE functions and make
+  // piece-meal changes to their code.
+  // The function is an atomic black-box.
+  // Solution: at least break it up into separate functions, as is now the case.
+  // Eventually, break up into atomic JS state operations:
+  // STATE-COPY o1 [ k1 ] <-- o2 [ k2 ] where o1,k1,o2,k2 are names
+  // METHOD INVOCATION: o.f(a1 ... aN)
+  // (CONDITIONAL) SEQUENCE: to be decided, but seems to consist
+  // of an extensional function to new instruction
+  // e.g.      if (a < 0) goto L
+  //      else if (a = 0) goto E
+  //      else if (a > 0) goto G
+  // is just an extensional function from the 3 possible values of sign(a):
+  // let s = sign(a)
+  // goto ( put s through the function:
+  //       -1 |--> L
+  //        0 |--> E
+  //        1 |--> G )
+  // We have gone from INFINITE SET (values of a)
+  // to FINITE SET (values of sign(a))
+  // to NEXT STATE-CHANGE (infinite set?)
+  // In summary: EXPOSE THE SUBSTRATE, part of which is JS itself.
+  vtable: {
+    ['clicked']: ({recv}, {dom_event}) => {
       // Create SVG circle and route keyboard input "to it"
-      let e = context.dom_event;
+      let e = dom_event;
       let circ = svgel('circle', svg, {r: 15, fill: 'red'});
       attribs(circ, {cx: e.offsetX, cy: e.offsetY});
       let obj = create_circle(circ);
       svg_userData(circ, obj);
       // Route keyboard input "to" the circle
       keyboard_focus = obj;
-    } else if (selector === 'being-considered') {
-    } else throw "Backg no comprende "+selector;
+    },
+    ['being-considered']: () => {},
   }
 });
 
 create_circle = (c) => {
   return {
     svgel: c,
-    receive: ({ recv, selector }, context) => {
-      if (selector === 'clicked') {
+    vtable: {
+      ['clicked']: ({recv}) => {
         send({ to: recv, selector: 'start-moving' });
-      } else if (selector === 'start-moving') {
+      },
+      ['start-moving']: ({recv}) => {
         let circ = recv.svgel;
         // Implement the initial conditions of the difference equation
         // center @ t+1 - center @ t = pointer @ t+1 - pointer @ t
@@ -172,19 +119,22 @@ create_circle = (c) => {
         // Enable the maintenance of this equality
         moving = recv;
         keyboard_focus = recv;
-      } else if (selector === 'being-moved') {
+      },
+      ['being-moved']: ({recv}, {vector}) => {
         let circ = recv.svgel;
         // Maintain center @ t+1 = center @ t + (pointer @ t+1 - pointer @ t)
-        let center_curr = add(recv.center_0, context.vector);
+        let center_curr = add(recv.center_0, vector);
         // Update the SVG dumb-state
         attr(circ, 'cx', center_curr[0]);
         attr(circ, 'cy', center_curr[1]);
-      } else if (selector === 'finish-moving') {
+      },
+      ['finish-moving']: ({recv}) => {
         // Halt maintenance of difference equation
         moving = undefined;
         recv.center_0 = undefined;
-      } else if (selector === 'key-down') {
-        let e = context.dom_event;
+      },
+      ['key-down']: ({recv}, {dom_event}) => {
+        let e = dom_event;
         if (recv.str === undefined) { // Lazy initialise text line on key input
           let [cx,cy] = [attr(recv.svgel, 'cx'), attr(recv.svgel, 'cy')];
           // Place text baseline and start point at circle center
@@ -201,40 +151,44 @@ create_circle = (c) => {
           });
         } else if (e.key.length === 1)
           send({ to: recv.str, selector: 'string-content' }, {string: s => s + e.key});
-      } else if (selector === 'being-considered') {
+      },
+      ['being-considered']: ({recv}, {truth}) => {
           // Early-bound one-element stack, lol
-          if (context.truth === true) { // PUSH...
+          if (truth === true) { // PUSH...
             recv.old_opacity = attr(recv.svgel, 'fill-opacity');
             attr(recv.svgel, 'fill-opacity', 0.5);
           } else { // ... POP!
             attr(recv.svgel, 'fill-opacity', recv.old_opacity);
           }
-      } else throw "Circle no comprende "+selector;
+      },
     }
   };
 };
 
 create_boxed_text = () => {
   let o = {
-    receive: ({ recv, selector }, context) => {
-      if (selector === 'created') {
+    vtable: {
+      ['created']: ({recv}) => {
         recv.text = svgel('text', svg, {x: 500, y: 500, font_size: 17, fill: 'white'});
         recv.rect = svgel('rect', svg, {fill_opacity: 0, stroke: 'gray'});
-      } else if (selector === 'string-content') {
-        let str = context.string;
+      },
+      ['string-content']: ({recv}, {string}) => {
+        let str = string;
         if (typeof(str) === 'undefined') str = recv.text.textContent;
         if (typeof(str) === 'function') str = str(recv.text.textContent);
         recv.text.textContent = str;
         send({ to: recv, selector: 'update-box' });
         return str;
-      } else if (selector === 'update-box') {
+      },
+      ['update-box']: ({recv}) => {
         let bbox = recv.text.getBBox();
         attribs(recv.rect, {x: bbox.x, y: bbox.y, width: bbox.width, height: bbox.height});
-      } else if (selector === 'set-baseline-start') {
-        let [x,y] = context.coords;
+      },
+      ['set-baseline-start']: ({recv}, {coords}) => {
+        let [x,y] = coords;
         attribs(recv.text, {x, y});
         send({ to: recv, selector: 'update-box' });
-      } else throw "Text no comprende "+selector;
+      }
     }
   };
   send({ to: o, selector: 'created' });
@@ -243,15 +197,16 @@ create_boxed_text = () => {
 
 create_signal = () => {
   let o = {
-    receive: ({ recv, selector }, context) => {
-      if (selector === 'created') {
-      } else if (selector === 'current-value') {
-        let v = context.set_to;
+    vtable: {
+      ['created']: ({recv}) => {
+      },
+      ['current-value']: ({recv}, {set_to}) => {
+        let v = set_to;
         if (typeof(v) === 'function') v = v(recv.value);
         // changed from recv.value to v 
         recv.value = v;
         return v;
-      }
+      },
     },
   };
   send({ to: o, selector: 'created' });
