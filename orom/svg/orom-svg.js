2e0d2b924aa91d583ba22a1b99f8f09e949f17a0
jdjakub <16978710+jdjakub@users.noreply.github.com>
2019-10-23 15:30:10 +0100

    Build off circles-text-etc live state infrastructure.

--- orom/svg/orom-svg.js
+++ orom/svg/orom-svg.js
@@ -38,6 +38,15 @@ svgel = (tag, parent, attrs) => {
   return elem;
 };
 
+state = function(o, k, v) {
+  let old = o[k];
+  if (v !== undefined) o[k] = v;
+  return old;
+}
+
+// Links an SVG node to its domain-level JS object
+svg_userData = (elem, obj) => state(elem, 'userData', obj);
+
 vadd = ([a, b], [c, d]) => [a+c, b+d];
 vsub = ([a, b], [c, d]) => [a-c, b-d];
 
@@ -54,239 +63,275 @@ bbox = el => {
   };
 }
 
+offset = e => [e.offsetX, e.offsetY]
+
 final = xs => xs[xs.length-1];
 
 htranslate = x => `translate(${x}, 0)`;
 vtranslate = y => `translate(0, ${y})`;
 translate = (x,y) => `translate(${x},${y})`;
 
-svg = svgel('svg', body);
-svg.style.border = '2px dashed red';
-
-resize = () => {
-  let dims = {width: body.offsetWidth*0.99, height: body.offsetHeight*0.99};
-  attr(svg, dims);
+send = ({ from, to, selector }, context) => {
+  // Allow objects to receive messages however they wish
+  // Treat absence of 'receive' as presence of 99%-case default
+  let next_code_path = to.receive || defaults.dyn_double_dispatch;
+  return next_code_path({ sender: from, recv: to, selector }, context || {});
 };
 
-window.onresize = resize;
-resize();
+defaults = { behavior: {} };
 
-pad = 4;
-col_width = 80;
-
-class CellGrid2D {
-
-  constructor(container, width, height) {
-    this.width = width || 8;
-    this.row_height = height || 30;
-    let top_left = svgel('g', container);
-
-    let big_rect = svgel('rect', top_left, {
-      x: 0, y: 0, width: pad+this.width+pad, height: this.row_height+pad,
-      'class': 'ent-handle'
-    });
-    let rows = svgel('g', top_left, {
-      'class': 'rows', transform: translate(pad,0)
-    });
-    this.top_left = top_left;
-    top_left.main_rect = big_rect;
-  }
-
-  newRow() {
-    let last_row = this.top_left.querySelector('.rows');
-    while (true) {
-      let next_row = last_row.querySelector('.row');
-      if (next_row) last_row = next_row;
-      else break;
+defaults.dyn_double_dispatch = ({ sender, recv, selector }, context) => {
+  let behaviors = recv.behaviors.concat([ defaults.behavior ]); // have at least default behaviour
+  for (let selector_to_next_code_path_or_behavior of behaviors) {
+    let next_code_path_or_behavior = selector_to_next_code_path_or_behavior[selector];
+    if (next_code_path_or_behavior === undefined) continue; // this behaviour doesn't deal with this type of input? Go to the next behaviour
+    else if (typeof(next_code_path_or_behavior) === 'function') {
+      let next_code_path = next_code_path_or_behavior;
+      return next_code_path({ sender, recv, selector }, context);
+    } else { // Assume further level of dispatch on sender - as a Map() rather than obj-dict
+      let sender_to_next_code_path = next_code_path_or_behavior;
+      let next_code_path = sender_to_next_code_path.get(sender);
+      if (next_code_path === undefined) continue;
+      else return next_code_path({ sender, recv, selector }, context);
     }
-    let new_row = svgel('g', last_row, {
-      'class': 'row', transform: vtranslate(this.row_height+pad)
-    });
-    svgel('rect', new_row, {
-      x: 0, y: 0, width: this.width, height: this.row_height
-    });
-    svgel('g', new_row, {
-      'class': 'cols', transform: translate(-col_width, pad)
-    });
-    attr(this.top_left.main_rect, 'height', h => +h+this.row_height+pad);
   }
 
-  newColumn() {
-    let prev_row = this.top_left.querySelector('.rows');
-    while (true) {
-      let curr_row = prev_row.querySelector('.row');
-      if (curr_row) {
-        let last_col = curr_row.querySelector('.cols');
-        while (true) {
-          let next_col = last_col.querySelector('.col');
-          if (next_col) last_col = next_col;
-          else break;
-        }
+  throw [`${recv} does not understand ${selector}`, recv, selector, context];
+};
 
-        let new_col = svgel('g', last_col, {
-          'class': 'col', transform: htranslate(pad+col_width)
-        });
-        svgel('rect', new_col, {
-          x: 0, y: 0, width: col_width, height: this.row_height-2*pad
-        });
-        attr(curr_row.querySelector('rect'), 'width', w => +w+pad+col_width);
+/* A Behavior is like an OROM vtable but without the possibility of fancy lookup
+ * semantics, for practical purposes.
+ *
+ * An OROM vtable is a messageable object that computably generates executable
+   code for "the current situation"'s short-term future time evolution.
+ * The "current situation" is a message receipt and includes the message
+ * selector, who sent it, the receiver, even the message-specific data...
+ *
+ * A Behaviour, on the other hand, is a data structure describing the same
+ * function, but without the privilege of computation. It stores short-term
+ * future time-evolution (STFTE?) code-paths indexed by message selector, and
+ * optionally the sender.
+ */
+behaviors = {}
 
-        prev_row = curr_row;
-      } else break;
-    }
-
-    attr(this.top_left.main_rect, 'width', w => +w+pad+col_width);
-  }
-}
-
-new_entity = () => {
-  let grid = svgel('g', svg);
-  grid.translate = [0,0];
-
-  let handle = svgel('rect', grid, {
-    'class': 'ent-handle',
-    x: 0, y: 0, width: 200, height: 20
-  });
-
-  let below = svgel('g', grid, {transform: 'translate(0, 20)'});
-
-  let mappings = svgel('g', below, {'class': 'mappings'});
-
-  let horizontals = svgel('g', mappings, {'class': 'horizontals'});
-  let verticals   = svgel('g', mappings, {'class': 'verticals'});
-
-  let input_column = svgel('g', horizontals, {transform: 'translate(20, 0)'});
-
-  let text = svgel('text', input_column, {
-    x: 0, y: 0, font_size: 20, font_family: 'Arial Narrow',
-    fill: 'gray', stroke: 'none'
-  });
-
-  text.textContent = 'New...';
-
-  // Left-align this text
-  let textbb = bbox(text);
-
-  let tl = [4,4];
-  let delta = vsub(tl, [textbb.l, textbb.t]); // curr TL --> desired TL corner
-  let [x, y] = vadd(xy(text), delta);         // corresponding new text origin
-
-  attr(text, {x, y}); // re-position text at new text origin
-  textbb = bbox(text); // get updated bbox
-  textbb.r += 4; // inflate bbox
-  textbb.b += 4;
-
-  let output_column = svgel('g', input_column, {transform: `translate(${4+textbb.w+4}, 0)`});
-
-  let output_ptr_handle = svgel('circle', horizontals, {
-    cx: 200-4-5, cy: textbb.b / 2, r: 5, fill: 'black'
-  });
-
-  // let cellGrid = new CellGrid(mappings);
-  // cellGrid.newRow();
-  // cellGrid.newColumn('handle');
-  // cellGrid.newColumn('input');
-  // cellGrid.newColumn('output');
-
-  let lleft    = svgel('line', verticals,     {x1: 0,      x2: 0,      y1: 0, y2: textbb.b});
-  let lright   = svgel('line', verticals,     {x1: 200,    x2: 200,    y1: 0, y2: textbb.b});
-  let linput   = svgel('line', input_column,  {x1: 0,      x2: 0,      y1: 0, y2: textbb.b});
-  let loutput  = svgel('line', output_column, {x1: 0,      x2: 0,      y1: 0, y2: textbb.b});
-  let lbottom  = svgel('line', horizontals, {y1: textbb.b, y2: textbb.b, x1: 0, x2: 200});
-
-  text.beginEdit = () => {console.log('Begin edit')};
-  text.finishEdit = () => {console.log('Finish edit')};
-
-  text.onmousedown = e => {
-    if (window.active_text !== undefined)
-        window.active_text.finishEdit();
-    window.active_text = text;
-    window.active_text.beginEdit();
+create = {};
+create.entity = (...behaviors) => {
+  let o = {
+    behaviors: [{}, ...behaviors],
+    //           ^ Entity's own personal behavior overrides specific to its instance
   };
-
-  return grid;
-}
-
-window.following_pointer = undefined;
-window.orig_pointer_pos = undefined;
-window.drag_meaning = 'move'
-window.quadrant = undefined;
-
-svg.onmousedown = e => {
-  let r = svg.getBoundingClientRect();
-  let [x, y] = vsub([e.clientX, e.clientY], [r.left, r.top]);
-  if (e.target === svg) {
-  } else if (e.target.tagName === 'rect') {
-    let r = e.target;
-    window.following_pointer = r;
-    window.orig_pointer_pos = [e.clientX, e.clientY];
-    let bb = bbox(r);
-    r.orig = {
-      translate: r.translate === undefined ? [0,0] : r.translate,
-      x: bb.left, y: bb.top, width: bb.width, height: bb.height
-    };
-  }
+  send({to: o, selector: 'created'});
+  return o;
 };
 
-svg.onmousemove = e => {
-  if (window.following_pointer !== undefined) {
-    let new_pointer_pos = [e.clientX, e.clientY];
-    let delta = vsub(new_pointer_pos, window.orig_pointer_pos);
-    let rect = window.following_pointer;
-    if (window.drag_meaning === 'move') {
-      rect.translate = vadd(rect.orig.translate, delta);
-      let [x,y] = rect.translate;
-      attr(rect, 'transform', translate(x,y));
-    } else if (window.drag_meaning === 'size') {
-      let [dx,dy] = delta;
-      attr(rect, {
-        width: rect.orig.width + dx,
-        height: rect.orig.height + dy
-      });
+//      ---***   OBSERVABLE   ***---
+// An Observable is live-state: a piece of state that notifies its dependents of change.
+behaviors.observable = {
+  ['created']: ({recv}) => {
+    recv.value  = () => recv._value;
+    recv.update =  v => recv._value = v;
+    recv._subs     = new Set();
+    recv.subs_copy = () => new Set(recv._subs);
+    recv.add    = sub => recv._subs.add(sub);
+    recv.remove = sub => recv._subs.delete(sub);
+  },
+  // I can be told that I changed to a new value, optionally including the old.
+  // If a function is specified as a new value, I treat this as a way to
+  // compute the new value from the old.
+  ['changed']: ({recv}, {from, to}) => {
+    let old_value = from;
+    let new_value = to;
+    if (old_value === undefined) old_value = recv.value(); // default to stored value
+    if (typeof(new_value) === 'function') new_value = new_value(old_value);
+    if (new_value !== old_value) {
+      recv.update(new_value); // if there is a difference, update self and dependents
+      for (let s of recv.subs_copy()) {
+        // notify each dependent that I changed, but use a copy of the list, so that
+        // anyone who wishes to un-subscribe in response, can do so safely
+        send({from: recv, to: s, selector: 'changed'}, {from: old_value, to: new_value});
+      }
+    }
+  },
+  ['poll']: ({recv}) => recv.value(), // returns mutable original...!
+  // Feels like setting and un-setting an "is-subscribed" observable...
+  ['subscribe-me']: ({sender, recv}) => {
+    recv.add(sender);
+  },
+  ['unsubscribe-me']: ({sender, recv}) => {
+    recv.remove(sender);
+  },
+};
+
+create.observable = () => create.entity(behaviors.observable);
+
+//      ---***   HAS-POSITION   ***---
+behaviors.has_position = {
+  ['created']: ({recv}) => {
+    recv.position = create.observable();
+  },
+  ['position']: ({recv}) => recv.position,
+};
+
+//      ---***   POINTER   ***---
+// Simulated presence of the human finger (or gaze).
+behaviors.pointer = {
+  ['created']: ({recv}) => {
+    recv.behaviors.push(behaviors.has_position); // Kludged dependence on another behaviour
+    behaviors.has_position['created']({recv}); // Hack in its initialisation...
+    // Which translates to wanting to send 'created' to it in the context of this receiver
+    // CODE INHERITANCE / CODE MIXINS ETC...
+
+    let pos = send({to: recv, selector: 'position'});
+    send({from: recv, to: pos, selector: 'subscribe-me'});
+
+    recv.currently_considering = create.observable();
+    send({from: recv, to: recv.currently_considering, selector: 'subscribe-me'});
+
+    recv.is_considering = new Map(); // Entity --> Observable
+  },
+  ['is-considering-me?']: ({sender, recv}) => {
+    // Problem: will prevent anything that ever asked this, from being GC'd.
+    // Suggestion: store this in the sender, instead of receiver...?
+    // A portion of an Entity (extensional function) used by another Entity.
+    // Like an ID card you need to carry round for the benefit of your
+    // institution, rather than ...?
+    // This is all because it's primarily a property OF sender:
+    // 'is-being-considered-by-pointer?', thus belongs with it.
+    // Another conundrum: where to store truly bidirectional relationships?
+    let is_considering_sender = recv.is_considering.get(sender);
+    if (is_considering_sender === undefined) {
+      is_considering_sender = create.observable();
+      recv.is_considering.set(sender, is_considering_sender);
+      send({to: is_considering_sender, selector: 'changed'}, {to: false});
+    }
+    return is_considering_sender;
+  },
+  ['is-considering']: ({recv}) => recv.currently_considering,
+  ['changed']: ({sender, recv}, context) => {
+    if (sender === recv.currently_considering) {
+      if (context.from !== undefined) {
+        // Spoof message send from the "no longer considered" Entity ... urgh
+        let no_longer_considering = send({from: context.from, to: recv, selector: 'is-considering-me?'});
+        send({to: no_longer_considering, selector: 'changed'}, {to: false});
+      }
+
+      if (context.to !== undefined) {
+        let now_considering = send({from: context.to, to: recv, selector: 'is-considering-me?'});
+        send({to: now_considering, selector: 'changed'}, {to: true});
+      }
     }
   }
 };
 
-svg.onmouseup = e => {
-  window.following_pointer = undefined;
+//      ---***   BACKGROUND   ***---
+behaviors.background = {
+  ['created']: ({recv}) => {
+   recv.rect = svgel('rect', svg, {x: 0, y: 0, fill: 'black'});
+   svg_userData(recv.rect, recv);
+
+   recv.dims = create.observable();
+   send({from: recv, to: recv.dims, selector: 'subscribe-me'});
+
+   recv.being_considered = send({from: recv, to: pointer, selector: 'is-considering-me?'});
+   send({from: recv, to: recv.being_considered, selector: 'subscribe-me'});
+
+   /* Initialise object's own specific 'changed' behaviour as a dispatch on
+      sender, where sender is compared to dynamic run-time properties of receiver
+      i.e. its "dims" or "being_considered" observables just created.
+    */
+
+   // First: lazy init the Map.
+   recv.behaviors[0]['changed'] = recv.behaviors[0]['changed'] || new Map();
+   let m = recv.behaviors[0]['changed'];
+   // Next: add the entries.
+
+   m.set(recv.being_considered, ({recv}, {to}) => {
+     if (to === true) // PUSH...
+       send({from: recv, to: left_mouse_button_is_down, selector: 'subscribe-me'});
+     else // ... POP!
+       send({from: recv, to: left_mouse_button_is_down, selector: 'unsubscribe-me'});
+   });
+
+   m.set(recv.dims, ({recv}, {to}) => {
+     attr(recv.rect, to);
+   });
+
+   m.set(left_mouse_button_is_down, ({recv}, {to}) => {
+     if (to === true) {
+       let pos = send({to: send({to: pointer, selector: 'position'}),
+                       selector: 'poll'});
+       console.log("LMB down: ", pos);
+     }
+    });
+
+    /*
+    Either we poll at runtime (if (sender === recv.being_considered)) every time,
+    or we construct an associative array that is only possible AFTER those are created.
+    This chosen latter solution relies on the requirement that the two observables
+    retain their identities throughout the background's lifetime, which is fair.
+    (as well as left_mouse_button_is_down)
+    */
+  }, /* At runtime this will be:
+  ['changed']: Map(
+    recv.being_considered     |-> ({recv}, {to}) => {...}
+    recv.dims                 |-> ({recv}, {to}) => {...}
+    left_mouse_button_is_down |-> ({recv}, {to}) => {...}
+  )
+  */
 };
 
-body.onkeydown = e => {
-  let t = window.active_text;
-  if (t === undefined) {
-    if (e.key === 's' && !e.repeat) {
-      window.drag_meaning = 'size';
-      console.log(window.drag_meaning);
-    } else return;
-    e.preventDefault();
-  } else {
-    if (e.key === 'Backspace') {
-      t.textContent = t.textContent.slice(0, -1);
-    } else if (e.key === 'Enter') {
-      t.finishEdit();
-    } else if (e.key.length === 1) {
-      t.textContent += e.key;
-    } else {
-      return;
-    }
-  }
-  e.preventDefault();
+pointer = create.entity(behaviors.pointer);
+
+/*
+ *  *** "DEVICE DRIVERS" FOR BINARY-STATE INPUT, POSITIONAL INPUT ***
+ */
+
+ svg = svgel('svg', body);
+ svg.style.border = '2px dashed red';
+
+left_mouse_button_is_down = create.observable();
+
+// Forget about coords; they are not part of the left button, or the keyboard, or the power button...
+svg.onmousedown = e =>
+  send({to: left_mouse_button_is_down, selector: 'changed'}, {to: true});
+
+svg.onmouseup = e =>
+  send({to: left_mouse_button_is_down, selector: 'changed'}, {to: false});
+
+// mousemove => pointer position changed
+svg.onmousemove = e =>
+  send({to: send({to: pointer, selector: 'position'}),
+        selector: 'changed'}, {to: offset(e)});
+
+// mouseover => considering a new object
+svg.onmouseover = e => {
+  let obj = svg_userData(e.target);
+  if (obj !== undefined)
+    send({ to: send({to: pointer, selector: 'is-considering'}),
+           selector: 'changed' }, { to: obj });
 };
 
-body.onkeyup = e => {
-  let t = window.active_text;
-  if (t === undefined) {
-    window.drag_meaning = 'move';
-    console.log(window.drag_meaning);
-  }
-  e.preventDefault();
-}
+// mouseout => no longer considering the object
+svg.onmouseout = e => {
+  let obj = svg_userData(e.target);
+  if (obj !== undefined)
+    send({ to: send({to: pointer, selector: 'is-considering'}),
+       selector: 'changed' }, { to: undefined });
+};
 
-new_entity();
-grid = new CellGrid2D(svg);
-grid.newRow();
-grid.newRow();
-grid.newRow();
-grid.newColumn();
-grid.newColumn();
-grid.newRow();
+/*   ^
+ *  /|\   Requires that mouseout occurs before mouseover.
+ * /_·_\  If not, will immediately stop considering new object.
+ *
+ */
+
+ backg = create.entity(behaviors.background);
+
+ resize = () => {
+   let dims = {width: body.offsetWidth*0.99, height: body.offsetHeight*0.99};
+   attr(svg, dims);
+   send({to: backg.dims, selector: 'changed'}, {to: dims});
+ };
+
+ window.onresize = resize;
+resize()
